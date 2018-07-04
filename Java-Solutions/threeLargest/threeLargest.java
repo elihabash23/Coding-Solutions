f2f44f1fc7c026c583491095db4f6bbe36ccb0e1
Elias Habash <eliashabash23@yahoo.com>
2018-07-04 02:24:51 -0700

    Return the three largest numbers in an unsorted array

--- Java-Solutions/threeLargest/threeLargest.java
+++ Java-Solutions/threeLargest/threeLargest.java
@@ -1,29 +1,33 @@
-// Find the 3 largest number in an array
+// Find the 3 largest numbers in an array
 
 public class threeLargest{
 
     public static int[] getThreeLargest(int[] nums) {
+        if (nums.length >= 3) {
 
-        int first = 0;
-        int second = 0;
-        int third = 0;
+            int first = 0;
+            int second = 0;
+            int third = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            
-            if(nums[i] > first) {
-                third = second;
-                second = first;
-                first = nums[i];
-            } else if (nums[i] > second) {
-                third = second;
-                second = nums[i];
-            } else if (nums[i] > third) {
-                third = nums[i];
+            for (int i = 0; i < nums.length; i++) {
+                
+                if(nums[i] > first) {
+                    third = second;
+                    second = first;
+                    first = nums[i];
+                } else if (nums[i] > second) {
+                    third = second;
+                    second = nums[i];
+                } else if (nums[i] > third) {
+                    third = nums[i];
+                }
             }
-        }
-        return new int[] {first, second, third};
-
+            return new int[] {first, second, third};
+    } else {
+        System.out.println("Size of array must be 3 or greater");
+        return null;
     }
+}
 
     public static void main(String[] args) {
         int[] nums = {200, 3, 5, 9, 2, 12, 45, 100, 55};
